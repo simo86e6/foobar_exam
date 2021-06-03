19151b9f6fa9c63872941702966ef86007717b42
simo86e6 <simo86e6@stud.kea.dk>
2021-06-03 18:56:22 +0200

    edit order

--- kurv.js
+++ kurv.js
@@ -1,6 +1,7 @@
 "use strict";
 import './style_form.scss'
 
+let beerList = JSON.parse(sessionStorage.getItem("selectedBeers"));
 let totalPrice = 0;
 document.querySelector("#buy_button").addEventListener("click", redirectPayment);
 basketSetup();
@@ -11,8 +12,8 @@ function redirectPayment(){
 }
 
 function basketSetup(){
-    debugger;
-    let beerList = JSON.parse(sessionStorage.getItem("selectedBeers"));
+    document.querySelector(".basket_container").innerHTML = "";
+    totalPrice = 0;
     let amountOfOrders = 0;
     
     beerList.forEach(beerOrder => {
@@ -23,7 +24,10 @@ function basketSetup(){
     });
 
     document.querySelector(".basket_total_orders").innerHTML = "DIN BESTILLING (" + amountOfOrders  + " varer)";
-    document.querySelector(".total_amount_right").innerHTML = totalPrice + " dkk"
+    document.querySelector(".total_amount_right").innerHTML = totalPrice + " dkk";
+
+    let beerListString = JSON.stringify(beerList);
+    sessionStorage.setItem("selectedBeers", beerListString);
 }
 
 
@@ -35,6 +39,22 @@ function showOrder(beerOrder){
     clone.querySelector(".basket_amount").textContent = "Antal: " + beerOrder.amount + " x " + beerOrder.price + " dkk";
     let lineTotal = beerOrder.amount * beerOrder.price;
     clone.querySelector(".basket_line_total").textContent = lineTotal + " dkk";
+    clone.querySelector(".counter_display").textContent = beerOrder.amount;
+
+
+    clone.querySelector(".counter_plus").addEventListener("click", function () {
+        beerOrder.amount++;
+        basketSetup();
+      });
+
+    clone.querySelector(".counter_minus").addEventListener("click", function () {
+        beerOrder.amount--;
+        basketSetup();
+      });
+
+
+
+
     document.querySelector(".basket_container").appendChild(clone);
     return lineTotal;
 
